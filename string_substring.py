fe589e8dd03909e9350897d6bf835c676eb6b3ee
kavya07k <111980449+kavya07k@users.noreply.github.com>
2024-04-17 21:29:42 +0530

    Create string_substring.py

---
+++ string_substring.py
@@ -0,0 +1,16 @@
+#The first line of input contains the original string. The next line contains the substring.
+
+def count_substring(string, sub_string):
+    total=0
+    for i in range(len(string)):
+        if string[i:len(string)].startswith(sub_string):
+            total += 1
+    return total
+    
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
